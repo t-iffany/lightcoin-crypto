360c45f324c8db077e6e113e2de0ac874023be6f
t-iffany <tiffanyxleong@gmail.com>
2022-11-26 00:46:23 +0000

    Implemented app to manage transactions for multiple accounts. Without extra features.

--- index.js
+++ index.js
@@ -1,29 +1,94 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+  // define a getter method called value
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("bob");
+
+console.log('Starting Balance:', myAccount.balance)
+
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+//console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+//console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(120.00, myAccount);
+t3.commit();
+//console.log('Transaction 3:', t3)
+
+console.log('Ending Balance:', myAccount.balance);
+
+
+
+// old code before introducing transaction class to share common code between Withdrawal and Deposit classes
+/*
+class Withdrawal {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance -= this.amount;
+  }
+
+}
+
+class Deposit {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance += this.amount;
+  }
+*/
